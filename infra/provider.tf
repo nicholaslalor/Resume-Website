8c00d62739e3574abb0eef70820939e9027ae7b1
Nicholas Lalor <nicholaslalor@gmail.com>
2024-10-17 16:00:43 -0400

    Updated provider.tf and front-end-cicd.yml for secret management

--- infra/provider.tf
+++ infra/provider.tf
@@ -7,13 +7,7 @@ terraform {
   }
 }
 
-# Define variables for AWS credentials
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-
 # Primary provider configuration for us-east-2
 provider "aws" {
-  region     = "us-east-2"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  region = "us-east-2"
 }
