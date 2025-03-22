e016109c206bf656eb72d5ade67f3c66a6829370
Nicholas Lalor <nicholaslalor@gmail.com>
2025-03-22 06:13:33 -0400

    ...

--- infra/provider.tf
+++ infra/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 }
 
-# Primary provider configuration for us-east-2
+# Primary provider configuration for us-east-2 (region will be fetched from environment variable)
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-2"  # You can still specify the region here or it will be picked up from AWS_DEFAULT_REGION if set in the environment variable
 }
